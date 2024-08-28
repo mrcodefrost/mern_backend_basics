a873a2e8f5cf21f1f856fee138fb8f328950a905
mrcodefrost <mr.codefrost@gmail.com>
2024-08-28 18:48:17 +0530

    middleware completed, routing basics started

--- Middleware/server.js
+++ Middleware/server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path');
 const cors = require('cors')
 const {logger} = require('./middleware/logEvents')
+const errorHandler = require('./middleware/logEvents');
+const { error } = require('console');
 const PORT = process.env.PORT || 3500;
 
 // Anything between requests and response are called Middlewares
@@ -24,7 +26,7 @@ app.use(logger)
 const whiteList = ['https://www.mysite.com', 'http://127.0.0.1:4500', 'http://localhost:3500']
 const corsOptions = {
     origin: (origin, callback) => {
-        if(whiteList.indexOf(origin) !== -1 || !origin){ // during dev add !origin
+        if(whiteList.indexOf(origin) !== -1 || !origin){ // during dev add !origin, allows undefined
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
@@ -97,20 +99,39 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three])
 
+// app.all v/s app.use
+// app.use does not allow regex and is more likely to be used for middlewares
+// app.all is used for routing, ideally app.all should be 
+// used for 404 
+
 // since express works like a waterfall, the last statement
 // would serve as a catch all / default page
 // sending 404 page not found will still give status code
 // as 200 since the file '404' was found, thus passing
 // a status code is required
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+
+// ------- Using app.get for 404 -------
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', '404.html'));
+// })
+
+// ------- Using app.all for 404 -------
+app.all('*', (req,res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({error: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not found')
+    }
 })
 
-// custom error builder middleware
-app.use((err,req,res, next) => {
-    console.error(err.stack)
-    res.status(500).send(err.message)
-})
+
+
+
+// custom error builder middleware ( keep in last )
+app.use(errorHandler)
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
